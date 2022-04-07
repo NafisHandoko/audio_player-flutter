206cb8444a4f5aae7d62cf9e6ae655969bacc70f
Nafis Handoko <naffisioner@gmail.com>
2022-04-07 12:36:37 +0700

    basic style and layout player.dart done

--- lib/screens/player.dart
+++ lib/screens/player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audio_player/global_styles.dart';
 
 class Player extends StatefulWidget {
   const Player({Key? key}) : super(key: key);
@@ -8,11 +9,48 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> {
+  double sliderValue = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
       child: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            // width: 278,
+            height: 278,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [boxshadow1],
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://images.unsplash.com/photo-1501472312651-726afe119ff1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'),
+                    fit: BoxFit.cover)),
+          ),
+          Container(
+            padding: const EdgeInsets.all(7),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle, color: color2, boxShadow: [boxshadow1]),
+            child: IconButton(
+                iconSize: 45,
+                onPressed: () {},
+                icon: Icon(
+                  Icons.play_arrow_rounded,
+                  color: Colors.white,
+                )),
+          ),
+          Slider(
+            min: 0.0,
+            max: 100.0,
+            value: sliderValue,
+            onChanged: (value) {
+              setState(() {
+                sliderValue = value;
+              });
+            },
+          )
+        ],
       ),
     );
   }
